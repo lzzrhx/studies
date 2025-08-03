4fa6df3aac790ad3f5390fc702f8bf55b44a1355
lzzrhx <lzzrhx@tuta.com>
2025-08-03 11:43:16 +0200

    edit

---
+++ 01_dataing/dat100/MalGrafikk.java
@@ -0,0 +1,11 @@
+public class Grafikk extends EasyGraphics {
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void run() {
+        makeWindow("Grafikk", 350, 150);
+        // drawCircle(150, 70, 60);
+    }
+}
